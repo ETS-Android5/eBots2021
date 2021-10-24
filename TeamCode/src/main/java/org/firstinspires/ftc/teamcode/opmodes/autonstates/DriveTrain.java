cc67b982ff4bf1588c76c48b6aab0440a60981d2
Op41Master <mgemel5576@rcs-k12.us>
2021-10-24 13:28:39 -0400

    Added new auton for scanning barCode.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonstates/DriveTrain.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.opmodes.autonstates;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class DriveTrain extends LinearOpMode {
+
+
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+
+
+    }
+}
