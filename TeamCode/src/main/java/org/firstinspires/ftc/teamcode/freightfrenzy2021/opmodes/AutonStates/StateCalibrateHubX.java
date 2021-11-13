be7b46c914882b5bd368a256fda7c15f033d580d
Brian Ignaczak <bignaczak@gmail.com>
2021-11-13 14:47:11 -0500

    Created CalibrateMovement and StateCalibrateHubX

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/freightfrenzy2021/opmodes/AutonStates/StateCalibrateHubX.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/freightfrenzy2021/opmodes/AutonStates/StateCalibrateHubX.java
@@ -2,15 +2,18 @@ package org.firstinspires.ftc.teamcode.freightfrenzy2021.opmodes.AutonStates;
 
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.ebotsenums.StartingSide;
+import org.firstinspires.ftc.teamcode.freightfrenzy2021.motioncontrollers.MecanumDrive;
+import org.firstinspires.ftc.teamcode.freightfrenzy2021.motioncontrollers.MecanumWheel;
 import org.firstinspires.ftc.teamcode.freightfrenzy2021.opmodes.EbotsAutonOpMode;
 import org.firstinspires.ftc.teamcode.ebotsutil.StopWatch;
 
 import java.util.ArrayList;
 
-public class StateMoveToHubX implements EbotsAutonState{
+public class StateCalibrateHubX implements EbotsAutonState{
 
     StopWatch stopWatch = new StopWatch();
     EbotsAutonOpMode autonOpMode;
@@ -26,7 +29,9 @@ public class StateMoveToHubX implements EbotsAutonState{
     private long driveTime;
 
 
-    public StateMoveToHubX(EbotsAutonOpMode autonOpMode){
+    public StateCalibrateHubX(EbotsAutonOpMode autonOpMode, MecanumWheel mecanumWheel){
+        mecanumWheel.zeroMotorEncoders();
+
         this.autonOpMode = autonOpMode;
         HardwareMap hardwareMap = autonOpMode.hardwareMap;
         frontLeft = hardwareMap.get(DcMotorEx.class, "frontLeft");
@@ -50,26 +55,33 @@ public class StateMoveToHubX implements EbotsAutonState{
             driveTime = 200;
             speed = -1.0;
         }
+
+
     }
 
-
-    @Override
+      @Override
     public boolean shouldExit() {
 
         boolean shouldExit = false;
 
-        if(stopWatch.getElapsedTimeMillis() >= driveTime){
+        if (autonOpMode.gamepad1.left_bumper && autonOpMode.gamepad1.right_bumper){
             shouldExit = true;
         }
-        return shouldExit | !autonOpMode.opModeIsActive();
 
+          autonOpMode.telemetry.update();
+        return shouldExit | !autonOpMode.opModeIsActive();
     }
 
     @Override
     public void performStateActions() {
-
-        for(DcMotorEx motor: motors) {
-            motor.setPower(speed);
+        if(stopWatch.getElapsedTimeMillis() <= driveTime){
+            for(DcMotorEx motor: motors) {
+                motor.setPower(speed);
+            }
+        } else {
+            for (DcMotorEx motor : motors) {
+                motor.setPower(0);
+            }
         }
     }
 
@@ -78,7 +90,12 @@ public class StateMoveToHubX implements EbotsAutonState{
             for(DcMotorEx motor: motors) {
                 motor.setPower(0.0);
             }
-
+    }
+    public void updateTelemetry(){
+        int i = 0;
+        for (DcMotorEx motor : motors){
+            autonOpMode.telemetry.addData("motor " + String.format("%d", i++), motor.getCurrentPosition());
+        }
 
     }
 }
