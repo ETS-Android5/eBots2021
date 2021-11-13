e9d0625f5414a5a282176cda5766022e602e52ef
Manjushri <manjsuhri.ug@gmail.com>
2021-11-13 14:52:08 -0500

    DriveAndSpin motion controller functionality

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/freightfrenzy2021/opmodes/AutonStates/StateOpenCVObserve.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.freightfrenzy2021.opmodes.AutonStates;
+
+import org.firstinspires.ftc.teamcode.freightfrenzy2021.opmodes.EbotsAutonOpMode;
+
+public class StateOpenCVObserve implements EbotsAutonState{
+
+    /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    Instance Attributes
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+    private EbotsAutonOpMode OpMode;
+
+    @Override
+    public boolean shouldExit() {
+        return false;
+    }
+
+    @Override
+    public void performStateActions() {
+
+    }
+
+    @Override
+    public void performTransitionalActions() {
+
+    }
+}
